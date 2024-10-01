e0a2725ba4828cbb72526f0c7a3362754a176077
kiri23 <christian@opticpower.com>
2024-10-01 14:54:44 -0700

    add binary

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c5017d4129902d290913776fbc41db8ebdbaf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d0b0b6447279c8939b61af40b902576d171bc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
